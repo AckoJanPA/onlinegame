6616caac32209892ca08b24ffc0545e676ffe830
AckoJanPA <ackojanpa@gmail.com>
2025-04-29 21:06:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function tos() {
     document.getElementById("viewport").innerHTML = `
-      <iframe src="0-1-0.html" width="75%" height="360">
-    `;
-    document.getElementById("sizeInput").innerHTML = `
-      <a href="0-1-0.html" target="_blank"><button>Open in new window</button></a>
-    `;
+      <iframe src="0-1-0.html" width="75%" height="360"></iframe><br>
+      <a href="0-1-0.html" target="_blank"><button>Open game</button></a>
+      <a href="SpriteEditor.html" target="_blank"><button class="center">Open Icon Editor</button></a><br>
+    `
+    document.getElementById("AgreeToToS").innerHTML = `
+      <a href="TermsOfService.txt" class="small" target="_blank">the terms of service</a>
+    `
 }
 
+// light mode toggle
 function lightMode() {
   document.getElementById("styles").innerHTML = `
     <link rel="stylesheet" href="light.css">
@@ -17,11 +20,19 @@ function lightMode() {
   `
 }
 
+// dark mode toggle
 function darkMode() {
   document.getElementById("styles").innerHTML = `
     <link rel="stylesheet" href="dark.css">
   `
   document.getElementById("dlMode").innerHTML = `
     <p class="small"><button onclick="lightMode()">Light Mode</button></p>
+  `
+}
+
+// shows Sprite Editor to the main window
+function showSpriteEditor() {
+  document.getElementById("spriteEditor").innerHTML = `
+    <iframe src="SpriteEditor.html" width="75%" height="360"></iframe>
   `
 }
